00a4bb25c6fbf396483026c7bbb56d6616a7a543
51pwn <18223385+hktalent@users.noreply.github.com>
2022-03-27 16:41:00 +0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# how build
+```bash
+pm install --save-dev @electron-forge/cli
+npx electron-forge import
+npm install
+npm run make
+ls -lah out/
+
+```
+
+# how run
+```bash
+npm start
+```
+
+
+# reference
+https://www.electronjs.org/docs/latest/tutorial/installation
---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(app.getAppPath(), 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => app.quit());
