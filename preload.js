7eb5fa1b57841b27483dd4b8b6dd8013a036f8f3
51pwn <18223385+hktalent@users.noreply.github.com>
2022-03-28 17:05:35 +0800

    add vue-admin

--- preload.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-  
-  
-  var o = document.getElementById('sOther')
-  for(var k in process.versions)
-  {
-    if(process.versions[k])o.innerText += "\n" + k + " = " + process.versions[k]
-  }
-})
