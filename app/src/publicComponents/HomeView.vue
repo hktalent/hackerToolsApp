b41b0fab294b8f63065386a8b7320c50a702fa63
51pwn <18223385+hktalent@users.noreply.github.com>
2022-05-14 23:34:46 +0800

    up

---
+++ app/src/publicComponents/HomeView.vue
@@ -0,0 +1,37 @@
+<template><iframe @src="ifrmSrc" @load="fnSetIp()" id="hktifrm"></iframe></template>
+
+<script>
+// @ is an alias to /src
+export default {
+  data () {
+    return {
+      ifrmSrc: ''
+    }
+  },
+  name: 'HomeView',
+  components: {
+  },
+  methods: {
+    fnSetIp () {
+      if (window.g_CurIps) {
+        let o = document.getElementById('hktifrm').contentWindow.document.getElementsByTagName('input')
+        if (o.length > 0) {
+          o = o[0]
+          if (o) {
+            o.value = window.g_CurIps[0] || ''
+          }
+        }
+      }
+    }
+  }
+}
+</script>
+<style>
+iframe {
+border:0;
+margin: 0;
+padding: 0;
+width:100%;
+height: calc(100%);
+}
+</style>
